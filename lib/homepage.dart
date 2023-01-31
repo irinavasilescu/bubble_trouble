e918862bccb564a3c2249f022e60fe2868075bac
Irina Vasilescu <irina>
2023-01-31 23:03:18 +0200

    init bubble trouble

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// It is a stateful widget because the game will have lots of moving parts
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // In the column will be two expanded widgets (one on top of each other)
+    // Instead of setting a fixed height for the containers, they will expand the container to fill all the space
+    return Column(
+      children: [
+        Expanded(
+            flex: 3, // This container takes up 75% of the space
+            child: Container(color: Colors.pink[100])),
+        Expanded(child: Container(color: Colors.grey))
+      ],
+    );
+  }
+}
